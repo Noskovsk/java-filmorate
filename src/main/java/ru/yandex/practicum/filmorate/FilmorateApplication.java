cb9fd533bd767640984d34449a67bf25c0132634
Noskovsk <dicksfriend@yandex.ru>
2022-08-07 13:54:26 +0300

    feat: сгенерировать приложение

---
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmorateApplication {
+
+	public static void main(String[] args) {
+		System.out.println("start");
+		SpringApplication.run(FilmorateApplication.class, args);
+	}
+
+}
