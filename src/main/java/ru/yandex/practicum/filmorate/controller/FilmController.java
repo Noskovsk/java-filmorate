817b9965345f21d9ca3b8f34dbb9f08cd309ad58
Noskovsk <dicksfriend@yandex.ru>
2022-08-25 09:45:21 +0300

    feat: сдаем финальный проект 10 спринта

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,18 +1,13 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Map;
-
 
 @RestController
 @RequestMapping("/films")
@@ -60,11 +55,5 @@ public class FilmController {
     @GetMapping("/popular")
     public List<Film> getTopPopularFilms(@RequestParam(name = "count", required = false) Integer count) {
         return filmService.getTopFilms((count == null || count <= 0) ? 10 : count);
-    }
-
-    @ExceptionHandler({ValidationException.class, MethodArgumentNotValidException.class})
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleArgumentNotValidException(final RuntimeException e) {
-        return Map.of("error", "Ошибка валидации параметра", "message", e.getMessage());
     }
 }
